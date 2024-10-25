f690c0198ad253d2e1e3c81c942e985f2c1b1f70
renkunx <renkunx@icloud.com>
2024-10-25 20:59:03 +0800

    feat: add dockerfile

--- server/config.js
+++ server/config.js
@@ -2,29 +2,10 @@ const CONF = {
     port: '5757',
     rootPathname: '',
 
-    // 微信小程序 App ID
-    appId: '',
-
-    // 微信小程序 App Secret
-    appSecret: '',
-
     // 是否使用腾讯云代理登录小程序
     useQcloudLogin: false,
     // 是否使用腾讯云 cos
     useQcloudCos: false,
-    /**
-     * MySQL 配置，用来存储 session 和用户信息
-     * 若使用了腾讯云微信小程序解决方案
-     * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
-     */
-    mysql: {
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        db: 'cAuth',
-        pass: 'wxb366c43b3a096ade',
-        char: 'utf8mb4'
-    },
 
     cos: {
         /**
@@ -42,4 +23,4 @@ const CONF = {
     wxLoginExpires: 7200
 }
 
-module.exports = process.env.NODE_ENV === 'local' ? Object.assign({}, CONF, require('./config.local')) : CONF;
+module.exports = process.env.NODE_ENV === 'local' ? Object.assign({}, CONF, require('./config.local')) : Object.assign({}, CONF, require('./config.prod'));
