8b958cb3f34c5b4aea7b2e9e50e7887bab5403b7
renkunx <renkunx@icloud.com>
2024-11-09 15:06:38 +0800

    feat: upload avatar

--- server/controllers/upload-local.js
+++ server/controllers/upload-local.js
@@ -1,30 +1,31 @@
-const { multiply } = require('lodash');
 const config = require('../config')
 module.exports =  {
     
     single: async(ctx)=>{
       if (!ctx.file) {
-        ctx.state.code = 400;
-        ctx.state.message = '没有文件被上传。';
+        ctx.status = 400;
+        ctx.body.message = '没有文件被上传。';
         return;
       }
-      ctx.state.data = {
+      ctx.body = {
+        code: 0,
         message: '文件上传成功',
         filename: ctx.file.filename,
-        path: ctx.file.path
+        path: `${config.serverHost}/uploads/${ctx.file.filename}`
       };
     },
     multiple: async(ctx)=>{
       if (!ctx.files || ctx.files.length === 0) {
-        ctx.state.code = 400;
-        ctx.state.message = '没有文件被上传。';
+        ctx.status = 400;
+        ctx.body.message = '没有文件被上传。';
         return;
       }
-      ctx.state.data = {
+      ctx.body = {
+        code: 0,
         message: '文件上传成功',
         files: ctx.files.map(file => ({
           filename: file.filename,
-          path: file.path
+          path: `${config.serverHost}/uploads/${file.filename}`
         }))
       };
     }
